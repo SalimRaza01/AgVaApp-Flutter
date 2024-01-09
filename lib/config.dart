e310d52088647f018cd6882a8b4abab7b575149c
SalimRaza01 <Salim.creation2017@gmail.com>
2024-01-09 18:04:26 +0530

    IP updated

--- lib/config.dart
+++ lib/config.dart
@@ -1,4 +1,6 @@
-final url = 'http://52.63.221.128:8000';
+// ignore_for_file: prefer_const_declarations
+
+final url = 'http://52.64.235.38:8000';
 final registerUser = '$url/api/logger/auth/register';
 final loginUser = '$url/api/logger/auth/login';
 final getDevicesByHospital = '$url/devices/get-devices-by-hospital';
