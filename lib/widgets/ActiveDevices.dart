b39f6d910f9d6b182597e0311e3ef0d18768eaaf
SalimRaza01 <Salim.creation2017@gmail.com>
2024-01-11 11:58:10 +0530

    Ui Improvement

--- lib/widgets/ActiveDevices.dart
+++ lib/widgets/ActiveDevices.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, sized_box_for_whitespace
 
 import 'package:flutter/material.dart';
 import '../Screens/DeviceDetails.dart';
@@ -32,7 +32,7 @@ class _ActiveDevicesState extends State<ActiveDevices> {
           child: Padding(
             padding: const EdgeInsets.only(top: 10),
             child: Container(
-              height: isExpanded ? 200 : 130,
+              height: isExpanded ? 250 : 130,
               width: 380,
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(16),
@@ -61,22 +61,37 @@ class _ActiveDevicesState extends State<ActiveDevices> {
 
   Widget buildCollapsedContent(Map<String, dynamic> deviceData) {
     return Padding(
-      padding: const EdgeInsets.only(top: 16, left: 16, right: 16),
+      padding: const EdgeInsets.only(top: 16, left: 30, right: 16),
       child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text(
-                deviceData['message'] ?? 'N/A',
-                style: TextStyle(
-                  fontFamily: 'Avenir',
-                  color: Color.fromARGB(255, 4, 75, 7),
-                  fontSize: 10,
-                ),
+              Row(
+                children: [
+                  Text(
+                    deviceData['message'],
+                    style: TextStyle(
+                      fontFamily: 'Avenir',
+                      color: Color.fromARGB(255, 4, 75, 7),
+                      fontSize: 12,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 10),
+                    child: Container(
+                      height: 10,
+                      width: 10,
+                      child: Image.asset(
+                         getImagePath(deviceData['message']),
+                      ),
+                    ),
+                  ),
+                ],
               ),
               Padding(
-                padding: const EdgeInsets.only(left: 15, top: 15, right: 50),
+                padding: const EdgeInsets.only(left: 0, top: 16, right: 0),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
@@ -91,7 +106,7 @@ class _ActiveDevicesState extends State<ActiveDevices> {
                     ),
                     SizedBox(height: 2),
                     Text(
-                      'Product Name',
+                      'Serial Number',
                       style: TextStyle(
                         fontFamily: 'Avenir',
                         color: const Color.fromARGB(255, 65, 65, 65),
@@ -104,20 +119,13 @@ class _ActiveDevicesState extends State<ActiveDevices> {
             ],
           ),
           Padding(
-            padding: const EdgeInsets.only(
-              top: 20,
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                Container(
-                  height: 60,
-                  width: 80,
-                  child: Image.asset(
-                    getImagePath(deviceData['deviceId']),
-                  ),
-                ),
-              ],
+            padding: const EdgeInsets.only(left: 50, bottom: 20),
+            child: Container(
+              height: 60,
+              width: 80,
+              child: Image.asset(
+                "assets/images/AgVaPro.png",
+              ),
             ),
           ),
         ],
@@ -128,191 +136,126 @@ class _ActiveDevicesState extends State<ActiveDevices> {
   Widget buildExpandedContent(Map<String, dynamic> deviceData) {
     return Row(
       children: [
-        Padding(
-          padding: const EdgeInsets.only(left: 20, top: 20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                deviceData['message'] ?? 'N/A',
-                style: TextStyle(
-                  fontFamily: 'Avenir',
-                  color: Color.fromARGB(255, 4, 75, 7),
-                  fontSize: 16,
-                ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Text(
-                deviceData['deviceId'],
-                style: TextStyle(
-                  fontFamily: 'Avenir',
-                  color: const Color.fromARGB(255, 65, 65, 65),
-                  fontSize: 12,
-                ),
-              ),
-                    SizedBox(
-                height: 10,
-              ),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            //showing other widgets
+            buildCollapsedContent(deviceData),
+            Padding(
+              padding: const EdgeInsets.only(left: 30, top: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Container(
-                    height: 40,
-                    width: 100,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.grey.withOpacity(0.3),
-                          spreadRadius: 1,
-                          blurRadius: 10,
-                          offset: Offset(0, 3),
-                        ),
-                      ],
-                      color: Color.fromARGB(255, 157, 0, 86),
-                    ),
-                    child: TextButton(
-                      onPressed: () {},
-                      style: TextButton.styleFrom(),
-                      child: Text(
-                        "Request",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 15,
-                        ),
-                      ),
+                  Text(
+                    'Device ID : ${deviceData['deviceId']}',
+                    // deviceData['deviceId'],
+                    style: TextStyle(
+                      fontFamily: 'Avenir',
+                      color: const Color.fromARGB(255, 65, 65, 65),
+                      fontSize: 12,
                     ),
                   ),
                   SizedBox(
-                    width: 110,
+                    height: 10,
                   ),
-                  Container(
-                    height: 40,
-                    width: 100,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.grey.withOpacity(0.3),
-                          spreadRadius: 1,
-                          blurRadius: 10,
-                          offset: Offset(0, 3),
-                        ),
-                      ],
-                      color: Color.fromARGB(255, 157, 0, 86),
+                  Text(
+                    'Last Hour : ${deviceData['last_hours']}',
+                    style: TextStyle(
+                      fontFamily: 'Avenir',
+                      color: Colors.red,
+                      fontSize: 12,
                     ),
-                    child: TextButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => DeviceDetails(deviceData),
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Container(
+                        height: 40,
+                        width: 100,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.3),
+                              spreadRadius: 1,
+                              blurRadius: 10,
+                              offset: Offset(0, 3),
+                            ),
+                          ],
+                          color: Color.fromARGB(255, 157, 0, 86),
+                        ),
+                        child: TextButton(
+                          onPressed: () {},
+                          style: TextButton.styleFrom(),
+                          child: Text(
+                            "Request",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 15,
+                            ),
                           ),
-                        );
-                      },
-                      style: TextButton.styleFrom(),
-                      child: Text(
-                        "More",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 15,
                         ),
                       ),
-                    ),
+                      SizedBox(
+                        width: 95,
+                      ),
+                      Container(
+                        height: 40,
+                        width: 100,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.3),
+                              spreadRadius: 1,
+                              blurRadius: 10,
+                              offset: Offset(0, 3),
+                            ),
+                          ],
+                          color: Color.fromARGB(255, 157, 0, 86),
+                        ),
+                        child: TextButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => DeviceDetails(deviceData),
+                              ),
+                            );
+                          },
+                          style: TextButton.styleFrom(),
+                          child: Text(
+                            "More",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 15,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ],
               ),
-            ],
-          ),
+            ),
+          ],
         ),
+        // ),
       ],
     );
   }
 
-// Padding(
-//           padding: const EdgeInsets.only(left: 70, right: 0, top: 20),
-//           child: Column(
-//             crossAxisAlignment: CrossAxisAlignment.end,
-//             children: [
-//               Container(
-//                 height: 40,
-//                 width: 100,
-//                 decoration: BoxDecoration(
-//                   borderRadius: BorderRadius.circular(10),
-//                   boxShadow: [
-//                     BoxShadow(
-//                       color: Colors.grey.withOpacity(0.3),
-//                       spreadRadius: 1,
-//                       blurRadius: 10,
-//                       offset: Offset(0, 3),
-//                     ),
-//                   ],
-//                   color: Color.fromARGB(255, 157, 0, 86),
-//                 ),
-//                 child: TextButton(
-//                   onPressed: () {},
-//                   style: TextButton.styleFrom(),
-//                   child: Text(
-//                     "Request",
-//                     style: TextStyle(
-//                       color: Colors.white,
-//                       fontSize: 15,
-//                     ),
-//                   ),
-//                 ),
-//               ),
-//               SizedBox(
-//                 height: 10,
-//               ),
-//               Container(
-//                 height: 40,
-//                 width: 100,
-//                 decoration: BoxDecoration(
-//                   borderRadius: BorderRadius.circular(10),
-//                   boxShadow: [
-//                     BoxShadow(
-//                       color: Colors.grey.withOpacity(0.3),
-//                       spreadRadius: 1,
-//                       blurRadius: 10,
-//                       offset: Offset(0, 3),
-//                     ),
-//                   ],
-//                   color: Color.fromARGB(255, 157, 0, 86),
-//                 ),
-//                 child: TextButton(
-//                   onPressed: () {
-//                      Navigator.push(
-//               context,
-//               MaterialPageRoute(
-//                 builder: (context) => DeviceDetails(deviceData),
-//               ),
-//             );
-//                   },
-//                   style: TextButton.styleFrom(),
-//                   child: Text(
-//                     "More",
-//                     style: TextStyle(
-//                       color: Colors.white,
-//                       fontSize: 15,
-//                     ),
-//                   ),
-//                 ),
-//               ),
-//             ],
-//           ),
-//         ),
-  String getImagePath(String deviceId) {
-    switch (deviceId) {
-      case '746ec924d3845797':
-        return "assets/images/Suction.png";
-      case '2ab16f2edef8891a':
-        return "assets/images/AgVaPro.png";
-      case '2ab16f2edef8891a':
-        return "assets/images/AgVaPro.png";
+  String getImagePath(String message) {
+    switch (message) {
+      case 'ACTIVE':
+        return "assets/images/active.png";
+      case 'INACTIVE':
+        return "assets/images/inactive.png";
       default:
-        return "assets/images/DVT.png";
+        return "assets/images/inactive.png";
     }
   }
 }
